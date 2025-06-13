a8687a437e0a93413562abd9ddd1852d9d8b45f7
Wendi <wendihan03@gmail.com>
2025-06-13 12:03:59 +0200

    refaire

---
+++ Database/Database.php
@@ -0,0 +1,16 @@
+<?php
+    class Database {
+        /**
+         * Lien avec la base de données (MySQL) (Utilisation des données dans le fichier .env Config perso)
+         * @return bool|mysqli
+         */
+        public function connect(){
+            $host =  getenv("MYSQL_HOST") == false ? 'localhost' : getenv("MYSQL_HOST") ;
+            $user = getenv("MYSQL_USER") == false ? 'root' : getenv("MYSQL_USER");
+            $pass = getenv("MYSQL_PASSWORD") == false ? '' : getenv("MYSQL_PASSWORD");;
+            $db = getenv("MYSQL_DATABASE") == false ? 'project_communication' : getenv("MYSQL_DATABASE");;
+            $port = 3306;   
+            $connection = mysqli_connect($host, $user, $pass, $db,$port);
+            return $connection;
+        }
+    }
